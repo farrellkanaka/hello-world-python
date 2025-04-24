f9ec20b9ce203a681fa71f3472b4ccd0f706e176
farrell kanaka <farrellkan@gmail.com>
2025-04-24 21:04:25 +0700

    Menambahkan fitur input pengguna untuk menyapa dengan nama

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,4 @@
 # hello_world.py
-
-# Tambahkan input pengguna untuk mendapatkan nama
-nama = input("Masukkan nama Anda: ")
-
-# Menampilkan pesan dengan nama yang dimasukkan oleh pengguna
-print(f"Hello, {nama}!")
+print("Halo, Dunia!")
+name = input("Siapa namamu? ")
+print(f"Halo, {name}!")
